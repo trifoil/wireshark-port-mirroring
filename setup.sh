cd5a695829975c9dec9356ffc7c43ea1ff4b9ee4
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-09-29 14:38:17 +0200

    Update setup.sh to install additional packages
    Added installation of nmtui and wireshark, and configured backports repository for cockpit.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nmtui -y
+sudo apt install wireshark -y
+
+. /etc/os-release
+echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" > \
+    /etc/apt/sources.list.d/backports.list
+apt update
+apt install -t ${VERSION_CODENAME}-backports cockpit
